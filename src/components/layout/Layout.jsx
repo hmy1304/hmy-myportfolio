0fdf1006e29752bf9ed57a2d376878b6a2fb6a84
unknown <hmy1304@kbu.ac.kr>
2026-05-14 18:17:02 +0900

    update

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Header from '../header/Header.jsx'
-import Footer from '../footer/Footer.jsx'
+import Header from '../Header/Header.jsx'
+import Footer from '../Footer/Footer.jsx'
 import styles from './Layout.module.scss'
 
 const Layout = () => {
